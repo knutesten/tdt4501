381905efb3d04892b847f69897185fcfca3c0d18
Knut Esten Melandsø Nekså <knutesten@hest.(none)>
2012-09-03 14:20:41 +0200

    Discovery of bluetooth devices added.

--- src/no/ntnu/falldetection/MainActivity.java
+++ src/no/ntnu/falldetection/MainActivity.java
@@ -1,20 +1,78 @@
 package no.ntnu.falldetection;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private int REQUEST_ENABLE_BT;
+	private ArrayAdapter<String> deviceArrayAdapter;
+	private ListView adapterList;
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			// When discovery finds a device
+			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+				// Get the BluetoothDevice object from the Intent
+				BluetoothDevice device = intent
+						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				// Add the name and address to an array adapter to show in a
+				// ListView
+				deviceArrayAdapter.add(device.getName() + "\n"
+						+ device.getAddress());
+			}
+		}
+	};
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		adapterList = (ListView) findViewById(R.id.adapterList);
+		deviceArrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+		adapterList.setAdapter(deviceArrayAdapter);
+
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			Log.d("BLUETOOTH", "NO BLUETOOTH");
+		} else if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+
+		// Register the BroadcastReceiver
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter); // Don't forget to unregister
+												// during onDestroy
+
+		mBluetoothAdapter.startDiscovery();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		deviceArrayAdapter.add("hest");
+		return true;
+	}
 }
